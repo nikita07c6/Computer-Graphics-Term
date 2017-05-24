a9342e6231cd274946b49fc21e53684f822f5f4f
DESKTOP-4BRK8M3\422 임성준 <sji3737@naver.com>
2017-05-24 20:01:19 +0900

    첫 커밋

---
+++ comgp1/Assets/Scripts/move.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour {
+    public GameObject bb;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.RotateAround(bb.transform.position, Vector3.up, 45*Time.deltaTime);
+	}
+}
