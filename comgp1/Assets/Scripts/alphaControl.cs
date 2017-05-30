0fbf10036cf80efed4e15eab4266ebaf6130a0d4
DESKTOP-4BRK8M3\422 임성준 <sji3737@naver.com>
2017-05-30 19:27:14 +0900

    투명껍질 구현

---
+++ comgp1/Assets/Scripts/alphaControl.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class alphaControl : MonoBehaviour {
+    public Renderer ren;
+    Color shellColor;
+    public float alpha;
+    public bool fade;
+
+	// Use this for initialization
+	void Start () {
+        ren.material.color = new Color(ren.material.color.r, ren.material.color.g, ren.material.color.b, 0.4f);
+        alpha = 0.4f;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        setFade();
+    }
+    public void fadeOut()
+    {
+        if (alpha > 0)
+        {
+            alpha -= 0.01f;
+            ren.material.color = new Color(ren.material.color.r, ren.material.color.g, ren.material.color.b, alpha);
+        }
+    }
+    public void fadeIn()
+    {
+        if (alpha < 0.4f)
+        {
+            alpha += 0.01f;
+            ren.material.color = new Color(ren.material.color.r, ren.material.color.g, ren.material.color.b, alpha);
+        }
+    }
+    public void setFade()
+    {
+        if (fade == false)
+        {
+            fadeIn();
+            
+        }
+        else
+        {
+            fadeOut();
+           
+        }
+    }
+    public void setFadeBool()
+    {
+        fade = !fade;
+    }
+}
