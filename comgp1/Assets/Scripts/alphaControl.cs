df25dc0dedccb1bac66a6c64612352594ae2a8f5
DESKTOP-4BRK8M3\422 임성준 <sji3737@naver.com>
2017-05-30 23:59:53 +0900

    껍질 3개 투명화 구현, 전자 4개 운동방향 다르게 해서 구현, move 스크립트 vector요소들을 x,y,z 변수로 받게 수정

--- comgp1/Assets/Scripts/alphaControl.cs
+++ comgp1/Assets/Scripts/alphaControl.cs
@@ -10,8 +10,8 @@ public class alphaControl : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        ren.material.color = new Color(ren.material.color.r, ren.material.color.g, ren.material.color.b, 0.4f);
-        alpha = 0.4f;
+        ren.material.color = new Color(ren.material.color.r, ren.material.color.g, ren.material.color.b, 0.0f);
+        alpha = 0.2f;
     }
 	
 	// Update is called once per frame
@@ -28,7 +28,7 @@ public class alphaControl : MonoBehaviour {
     }
     public void fadeIn()
     {
-        if (alpha < 0.4f)
+        if (alpha < 0.2f)
         {
             alpha += 0.01f;
             ren.material.color = new Color(ren.material.color.r, ren.material.color.g, ren.material.color.b, alpha);
@@ -36,7 +36,7 @@ public class alphaControl : MonoBehaviour {
     }
     public void setFade()
     {
-        if (fade == false)
+        if (fade == true)
         {
             fadeIn();
             
--- comgp1/Assets/Scripts/move.cs
+++ comgp1/Assets/Scripts/move.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class move : MonoBehaviour {
     public GameObject bb;
-	// Use this for initialization
-	void Start () {
+    public float x;
+    public float y;
+    public float z;
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        transform.RotateAround(bb.transform.position, Vector3.up, 45*Time.deltaTime);
+        transform.RotateAround(bb.transform.position, new Vector3(x, y, z), 45*Time.deltaTime);
 	}
 }
