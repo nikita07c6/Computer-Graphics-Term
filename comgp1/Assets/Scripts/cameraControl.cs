4d9edca53f5f70e980f29673e47ab21c7cb35f3d
DESKTOP-4BRK8M3\422 임성준 <sji3737@naver.com>
2017-06-01 03:41:53 +0900

    화면회전, 확대축소, 화면리셋 기능 구현. 버튼 위치 및 크기 변경

---
+++ comgp1/Assets/Scripts/cameraControl.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraControl : MonoBehaviour
+{
+
+    public Transform target;
+
+    Transform camera;
+
+    // Use this for initialization
+    void Start()
+    {
+        camera = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 dist = camera.position - target.position;
+        dist = Vector3.Normalize(dist);
+        if (Input.GetMouseButton(0))
+        {
+            transform.RotateAround(target.position, Vector3.left, Input.GetAxis("Mouse Y") * 10f);
+            transform.RotateAround(target.position, Vector3.up, Input.GetAxis("Mouse X") * 10f);
+        }
+        camera.position = camera.position - dist * Input.GetAxis("Mouse ScrollWheel") * 10f;
+    }
+    public void positionReset()
+    {
+        camera.position = target.position + Vector3.back * 15f;
+        camera.LookAt(target);
+    }
+}
